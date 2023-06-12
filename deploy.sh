3dea13e730551843e02b2eed5ebd12b2718e76af
clean17 <piw940317@gmail.com>
2023-06-13 01:47:09 +0900

    CI/CD 테스트 04

--- deploy.sh
+++ deploy.sh
@@ -23,18 +23,10 @@ if ! type docker > /dev/null
 then
   echo "docker does not exist"
   echo "Start installing docker"
-  sudo apt-get update
-  sudo apt-get install -y \
-      apt-transport-https \
-      ca-certificates \
-      curl \
-      gnupg-agent \
-      software-properties-common
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+  sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-     $(lsb_release -cs) \
-     stable"
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 fi
