e9253290ffea1164f33e6bbcb66c69466cea85f9
clean17 <piw940317@gmail.com>
2023-06-13 05:49:22 +0900

    CI/CD 테스트 06

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@
 
 if ! type docker > /dev/null
 then
-  echo "docker does not exist"
+  echo "================================ docker does not exist ==============================="
   echo "Start installing docker"
   sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
   sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
@@ -64,7 +64,7 @@ fi
 # Docker Buildx 설치 확인
 if ! docker buildx version > /dev/null
 then
-  echo "Docker Buildx does not exist"
+  echo "=============================== Docker Buildx does not exist ====================================="
   echo "Start installing Docker Buildx"
   # Docker Buildx 바이너리 다운로드
   BUILDX_VERSION="v0.6.3"
@@ -80,13 +80,12 @@ fi
 # Installing docker-compose if not exists
 if ! type docker-compose > /dev/null
 then
-  echo "docker-compose does not exist"
+  echo "============================ docker-compose does not exist ======================================"
   echo "Start installing docker-compose"
   sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
-echo "start docker-compose up: ubuntu"
+echo "============================== start docker-compose up: ubuntu ================================="
 sudo COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose-prod.yml up --build -d
-
 # .env 파일로 환경변수를 ec2에 전달하려면 실행하는 스크립트가 docker-compose와 같은 디렉토리에 있어야 한다.
