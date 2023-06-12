7c538921960e446beb572cb9c87f9513352186c0
clean17 <piw940317@gmail.com>
2023-06-13 07:54:03 +0900

    ec2 배포  테스트

--- deploy.sh
+++ deploy.sh
@@ -32,8 +32,8 @@ if ! type docker-compose > /dev/null
 then
   echo "============================ docker-compose does not exist ======================================"
   echo "Start installing docker-compose"
-  sudo curl -L "https://github.com/docker/compose/releases/download/2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose # https://github.com/docker/compose#where-to-get-docker-compose 참고
-  sudo chmod 664 /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose # https://github.com/docker/compose#where-to-get-docker-compose 참고
+  sudo chmod -x /usr/local/bin/docker-compose
 fi
 
 echo "============================== start docker-compose up: ubuntu ================================="
